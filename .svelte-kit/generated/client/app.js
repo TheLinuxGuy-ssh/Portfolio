a10ad3f88c6935dc87d4afb57e4d292831a0ce9f
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-08-04 20:05:38 +0530

    New Projects Added

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -14,16 +14,18 @@ export const nodes = [
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
-	() => import('./nodes/5')
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
 		"/": [2],
-		"/hyprfect": [3],
-		"/matrix": [4],
-		"/matrix/post/[page]": [5]
+		"/duskmeter": [3],
+		"/hyprfect": [4],
+		"/matrix": [5],
+		"/matrix/post/[page]": [6]
 	};
 
 export const hooks = {
--- .svelte-kit/non-ambient.d.ts
+++ .svelte-kit/non-ambient.d.ts
@@ -29,18 +29,19 @@ declare module "$app/types" {
 	type MatcherParam<M> = M extends (param : string) => param is (infer U extends string) ? U : string;
 
 	export interface AppTypes {
-		RouteId(): "/" | "/hyprfect" | "/matrix" | "/matrix/post" | "/matrix/post/[page]";
+		RouteId(): "/" | "/duskmeter" | "/hyprfect" | "/matrix" | "/matrix/post" | "/matrix/post/[page]";
 		RouteParams(): {
 			"/matrix/post/[page]": { page: string }
 		};
 		LayoutParams(): {
 			"/": { page?: string | undefined };
+			"/duskmeter": Record<string, never>;
 			"/hyprfect": Record<string, never>;
 			"/matrix": { page?: string | undefined };
 			"/matrix/post": { page?: string | undefined };
 			"/matrix/post/[page]": { page: string }
 		};
-		Pathname(): "/" | "/hyprfect" | "/matrix" | `/matrix/post/${string}` & {};
+		Pathname(): "/" | "/duskmeter" | "/hyprfect" | "/matrix" | `/matrix/post/${string}` & {};
 		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
 		Asset(): "/Desktop.mp4" | "/audio/boot.mp3" | "/css/links.css" | "/css/locomotive.css" | "/css/normalize.css" | "/css/style.css" | "/fonts/Hermit/Hermit-Light.woff2" | "/fonts/Hermit/LICENSE" | "/fonts/ProFont/LICENSE" | "/fonts/ProFont/ProFont.woff2" | "/hyprfect.sh" | "/images/general/logo.webp" | "/images/projects/COSMA 2023.mp4" | "/images/projects/COSMA2k24.mp4" | "/images/projects/DarwinX.mp4" | "/images/projects/DigiPod.mp4" | "/images/projects/ESP32 Irrigation Automation.mp4" | "/images/projects/Global Insulation.mp4" | "/images/projects/Hyprfect.mp4" | "/images/projects/JP Academy.mp4" | "/images/projects/Kepler.mp4" | "/images/projects/Mahaveer International School.mp4" | "/images/projects/Marine Consultancy Group.mp4" | "/images/projects/SkillHiive Stack.mp4" | "/images/projects/Sosh Media.mp4" | "/images/projects/StarWalk CA.mp4" | "/images/projects/StarWalk Core.mp4" | "/images/projects/TLG Links.mp4" | "/images/projects/TLG Start Page.mp4" | "/images/projects/Tagged.mp4" | "/images/projects/The Spartan Rage.mp4" | "/images/projects/Unite.Do.mp4" | "/images/svg/mail.svg" | "/svelte.svg" | "/tlglink.mp4" | "/vite.svg" | string & {};
 	}
